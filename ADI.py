44763ad6805eb53fb8a13b22449e7371fba469ad
robbiejones96 <robbiejones96@gmail.com>
2018-10-26 03:47:08 -0700

    Add isSolved function

--- ADI.py
+++ ADI.py
@@ -1,8 +1,28 @@
 import py222
 from random import randint, getrandbits
-
+import numpy as np
 
 moves = ['F', 'B', 'R', 'L', 'D', 'U']
+
+
+def generateSolvedCubes():
+    solvedCubes = [None] * 7
+    solvedCubes[0] = py222.initState()
+    solvedCubes[1] = py222.doAlgStr(solvedCubes[0], 'y')
+    solvedCubes[2] = py222.doAlgStr(solvedCubes[1], 'y')
+    solvedCubes[3] = py222.doAlgStr(solvedCubes[2], 'y')
+    solvedCubes[4] = py222.doAlgStr(solvedCubes[0], 'x')
+    solvedCubes[5] = py222.doAlgStr(solvedCubes[4], 'x')
+    solvedCubes[6] = py222.doAlgStr(solvedCubes[5], 'x')
+    return solvedCubes
+
+solvedCubes = generateSolvedCubes()
+
+def isSolved(cube):
+    for solvedCube in solvedCubes:
+        if np.array_equal(cube, solvedCube):
+            return True
+    return False
 
 def getRandomMove():
     move = moves[randint(0, 5)]
@@ -26,3 +46,4 @@ def getChildren(cube):
     for i in range(len(moves)):
         children[i + len(moves)] = py222.doAlgStr(cube, moves[i] + '\'')
     return children
+
