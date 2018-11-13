7dd1e3a90ddb785c04c5a53ee90caa3c5fe4af16
robbiejones96 <robbiejones96@gmail.com>
2018-11-13 02:18:33 -0800

    Write ADI skeleton

--- ADI.py
+++ ADI.py
@@ -1,8 +1,8 @@
 import py222
-from random import randint, getrandbits
+from random import randint
 import numpy as np
 
-moves = ['F', 'B', 'R', 'L', 'D', 'U']
+moves = ['F', 'F\'', 'B', 'B\'', 'R', 'R\'', 'L', 'L\'', 'D', 'D\'', 'U', 'U\'']
 
 
 def generateSolvedCubes():
@@ -25,9 +25,9 @@ def isSolved(cube):
     return False
 
 def getRandomMove():
-    move = moves[randint(0, 5)]
-    return move + '\'' if getrandbits(1) else move
+    return moves[randint(0, len(moves) - 1)]
 
+# TODO: Add the loss weight to each sample?
 def generateSamples(k, l):
     N = k * l
     samples = [None] * N
@@ -40,10 +40,39 @@ def generateSamples(k, l):
     return samples
         
 def getChildren(cube):
-    children = [None] * len(moves) * 2
+    children = [None] * len(moves)
     for i in range(len(moves)):
         children[i] = py222.doAlgStr(cube, moves[i])
-    for i in range(len(moves)):
-        children[i + len(moves)] = py222.doAlgStr(cube, moves[i] + '\'')
     return children
+
+def reward(cube):
+    return 1 if isSolved(cube) else -1
+
+def forwardPass(cube):
+    # TODO: run cube through neural net
+
+    return 0, np.empty(12) # TODO: REMOVE THIS
+
+def train(samples , optimalVals, optimalPolicies):
+    pass
+
+def doADI(k, l, M):
+    for _ in range(M):
+        samples = generateSamples(k, l)
+        optimalVals = np.empty(len(samples))
+        optimalPolicies = np.empty((len(samples), len(moves)))
+        for i, sample in enumerate(samples):
+            children = getChildren(sample)
+            values = np.empty(len(children))
+            policies = np.empty((len(children), len(moves)))
+            for j, child in enumerate(children):
+                value, policy = forwardPass(child)
+                values[j] = value
+                policies[j] = policy
+            optimalVals[i] = values.max()
+            optimalPolicies[i] = policies[values.argmax()]
+        train(samples, optimalVals, optimalPolicies)
+
+
+
 
