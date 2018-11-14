f43c71ee7def6216c2685b66db5a92a10ff9d4c3
robbiejones96 <robbiejones96@gmail.com>
2018-11-14 13:22:39 -0800

    First draft at ADI training

--- ADI.py
+++ ADI.py
@@ -1,8 +1,11 @@
 import py222
 from random import randint
 import numpy as np
+import tensorflow as tf
 
 moves = ['F', 'F\'', 'B', 'B\'', 'R', 'R\'', 'L', 'L\'', 'D', 'D\'', 'U', 'U\'']
+
+kLearningRate = 0.75
 
 def getRandomMove():
     return moves[randint(0, len(moves) - 1)]
@@ -22,32 +25,68 @@ def generateSamples(k, l):
 def reward(cube):
     return 1 if py222.isSolved(cube, True) else -1
 
-def forwardPass(cube):
-    # TODO: run cube through neural net
-    return 0, np.empty(12) # TODO: REMOVE THIS
+def forwardPass(cube, sess):
+    state = py222.getState(cube).flatten()
+    inputState = np.array([state])
+    policy = sess.run(policyOutput, feed_dict={X: inputState})
+    policy = tf.nn.softmax(policy)
+    value = sess.run(valueOutput, feed_dict={X: inputState})
+    return value, policy
 
-def train(states, optimalVals, optimalPolicies):
-    pass
+def train(states, optimalVals, optimalPols, sess):
+    optimalVals = np.array([optimalVals]).T
+    sess.run(optimizer, feed_dict={X : states, optimalPolicies : optimalPols, optimalValues : optimalVals})
+
+def constructGraph():
+    global X
+    global optimalPolicies
+    global optimalValues
+    global layer1
+    global layer2
+    global policyLayer1
+    global valueLayer1
+    global policyOutput
+    global valueOutput
+    global policyLoss
+    global valueLoss
+    global optimizer
+
+    X = tf.placeholder(tf.float32, shape=[None, 8 * 24], name="Cube")
+    layer1 = tf.contrib.layers.fully_connected(X, 4096, tf.nn.elu, weights_initializer=tf.glorot_uniform_initializer)
+    layer2 = tf.contrib.layers.fully_connected(layer1, 2048, tf.nn.elu, weights_initializer=tf.glorot_uniform_initializer)
+    policyLayer1 = tf.contrib.layers.fully_connected(layer2, 512, tf.nn.elu, weights_initializer=tf.glorot_uniform_initializer)
+    valueLayer1 = tf.contrib.layers.fully_connected(layer2, 512, tf.nn.elu, weights_initializer=tf.glorot_uniform_initializer)
+    policyOutput = tf.contrib.layers.fully_connected(policyLayer1, 12, activation_fn=None, weights_initializer=tf.glorot_uniform_initializer)
+    valueOutput = tf.contrib.layers.fully_connected(valueLayer1, 1, activation_fn=None, weights_initializer=tf.glorot_uniform_initializer)
+    optimalPolicies = tf.placeholder(tf.float32, shape=[None, 12], name="optimalPolicies")
+    optimalValues = tf.placeholder(tf.float32, shape=[None, 1], name="OptimalValues")
+    policyLoss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=optimalPolicies, logits=policyOutput)
+    valueLoss = tf.losses.mean_squared_error(optimalValues, valueOutput)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=kLearningRate).minimize(valueLoss + policyLoss)
+
 
 # TODO: might need to change numpy arrays to TF variables
 def doADI(k, l, M):
+    constructGraph()
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
     for _ in range(M):
         samples = generateSamples(k, l)
         states = np.empty((len(samples), 8 * 24))
         optimalVals = np.empty(len(samples))
-        optimalPolicies = np.empty((len(samples), len(moves)))
+        optimalPolicies = np.empty((len(samples), len(moves)))  
         for i, sample in enumerate(samples):
             values = np.empty(len(moves))
             for j, move in enumerate(moves):
                 child = py222.doAlgStr(sample, move)
-                value, policy = forwardPass(child)
+                value, _ = forwardPass(child, sess)
                 values[j] = value + reward(child)
             optimalVals[i] = values.max()
             oneHot = np.zeros(len(moves))
             oneHot[values.argmax()] = 1
             optimalPolicies[i] = oneHot
             states[i] = py222.getState(sample).flatten()
-        train(states, optimalVals, optimalPolicies)
+        train(states, optimalVals, optimalPolicies, sess)
 
 
 
